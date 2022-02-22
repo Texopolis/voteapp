b22e5da03f1f5b80344433821e4b5982391fbd49
Douglas B <guod1977@gmail.com>
2022-02-21 23:48:11 -0800

    working interaction and ui

--- src/App.js
+++ src/App.js
@@ -1,27 +1,125 @@
 import './App.css';
 import React from 'react';
 import { useEffect, useState } from 'react';
+import { ethers } from 'ethers'
+import abi from './Vote.json'
 import { 
-  contract,
-  currentVoteCount,
-  connectWallet,
-  vote,
-  updateVoteCount,
-  getCurrentWalletConnected
-} from './util/interact';
-
-//State variables
-const [walletAddress, setWallet] = useState("");
-const [status, setStatus] = useState("");
-const [message, setMessage] = useState("No connection to the network.");
-const [newMessage, setNewMessage] = useState("");
-
+  checkWalletIsDetected,
+  connectWalletHandler,
+  } from './util/interact';
+import candidate1img from './images/candidate1.png';
+import candidate2img from './images/candidate2.png';
 
 function App() {
 
+//State variables
+const [walletAddress, setWalletAddress] = useState("");
+const [message, setMessage] = useState("No connection to the network.");
+const [walletDetected, setWalletDetected] = useState("false")
+
+const contractABI = abi.abi
+const contractAddress = '0x5187B771259e4E7E7334616bb0290C130983FE98'
+
+const { ethereum } = window;
+
+const provider = new ethers.providers.Web3Provider(ethereum);
+const signer = provider.getSigner();
+const contract = new ethers.Contract( contractAddress, contractABI, signer);
+
+const [candidate1, setCandidate1]= useState("")
+const [candidate2, setCandidate2]= useState("")
+
+//connect to wallet
+const connectWallet = async () =>{
+  setWalletAddress(await connectWalletHandler())
+  setMessage("connected")
+};
+console.log(walletAddress)
+
+//wallet changes
+const addWalletListener = ()=> {
+  if (ethereum){
+    ethereum.on("accountsChanged", (accounts)=>{
+      if (accounts.length !== 0){
+        setWalletAddress(accounts[0])
+        setMessage("account changed successfully")
+      }
+    })
+  }
+};
+
+
+
+  //read the contract
+useEffect(() => {
+  const fetchCandidate= async () => {
+    const res1 = await contract.candidateLookup(1)
+    setCandidate1(res1)
+    const res2 = await contract.candidateLookup(2)
+    setCandidate2(res2)
+   };
+  fetchCandidate()
+}, [])
+
+
+
+//vote function
+const vote = async (id) =>{
+  await contract.vote(id)
+}
+
+const [voteCount1, setVoteCount1]= useState(0)
+const [voteCount2, setVoteCount2]= useState(0)
+
+
+const getVote1 = async () => {
+  let voteCount1 = await contract.candidateLookup(0)
+  setVoteCount1(voteCount1[2].toNumber())
+}
+
+const getVote2 = async () => {
+  let voteCount2 = await contract.candidateLookup(1)
+  setVoteCount2(voteCount2[2].toNumber())
+}
+
+
+const addSmartContractListener = async () => {
+  contract.on('voterEvent', getVote1)
+  contract.on('voterEvent', getVote2)
+};
+
+
+useEffect(() => {
+  // connectToContract()
+  setWalletDetected(checkWalletIsDetected()) 
+  addWalletListener()
+  addSmartContractListener()
+},[]);
+
   return (
-    <div>
+    <div className="app">
+      <div className="top">
+        
+            <h3>{message}</h3>
+          {walletDetected && (
+            <button className="button-32" onClick = {connectWallet}>
+              {walletAddress.length > 0 ? 
+              ('Connected: ' + String(walletAddress).substring(0,6)+ "..." + String(walletAddress).substring(38)) :
+              (<span>Connect Wallet</span>)}
+            </button>)}
+        
+      </div>
+      
       <h1>VOTE HERE</h1>
+      
+      
+      <div className='main-content'>
+        <div className='left'>{candidate1[1]}  {voteCount1}</div>
+        <div className='right'>{candidate2[1]}  {voteCount2}</div>
+        <button className="button-32" onClick= {()=>{vote(0); getVote1()}}>VOTE joey</button>
+        <button className="button-32" onClick= {()=>{vote(1); getVote2()}}>VOTE sarah</button>
+
+      </div>
     </div>
   );
 }
--- src/util/interact.js
+++ src/util/interact.js
@@ -1,134 +1,134 @@
-export const contract;
 
-export const currentVoteCount = async () =>{
-
-}
-
-export const connectWallet = async () =>{
-
-}
-
-const getCurrentWalletConnected = async () =>{
-
-}
-
-export const vote = async () =>{
-
-}
-
-export const updateVoteCount = async () => {
-
-}
-
-
-
-///////////////////////////////////////////////////////////////////
-import abi from './Vote.json';
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers'
-
-
-const ABI = abi.abi;
-const ADDRESS = "0x98B3cDD6e2fE44A3f2fCAfFb036c20f6E6595709"
-;
 const { ethereum } = window;
-let provider = null;
-let signer = null;
-let contract = null;
-let candidate1= [];
-let candidate2 = null;
 
+//check for wallet in browser and if present display button to connect
+export const checkWalletIsDetected = async () => {
+    if (!ethereum) {
+      console.log("Make sure you have Metamask installed")
+      return false;
+    } else {
+      console.log('Wallet detected- let"s do this!')
+      return true
+    }}
+ 
+    
 
-const [currentAccount, setCurrentAccount] = useState(null);
-const [contractConnected, setContractConnected] = useState(true);
-
-//check for wallet in browser
-const checkWalletIsConnected = async () => {
-  if (!ethereum) {
-    console.log("Make sure you have Metamask installed")
-    return;
-  } else {
-    console.log('Wallet detected- let"s do this!')
-  }
-
-  const accounts = await ethereum.request({ method: 'eth_accounts' });
-
-  if (accounts.length != 0) {
-    const account = accounts[0]
-    console.log('Found authorized account: ', account);
-  } else {
-    console.log('No authorized account found');
-  }
-}
-
-//connect wallet- need to be a button Function
-const connectWalletHandler = async ()=>{
+//connect wallet- needs to be a button Function
+export const connectWalletHandler = async ()=>{
   if (!ethereum){
     alert("Please install Metamask")
+    return;
   }
-
   try {
-    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-    setCurrentAccount(accounts[0]);
-    console.log('Using account at address: ', currentAccount);
+    await ethereum.request({ method: 'eth_requestAccounts' });
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    if (accounts.length != 0) {
+        const account = await accounts[0]
+        console.log("account at", account);
+        return account
+    } else {
+        console.log('No authorized account found');
+    }
   }
   catch (error) {
     console.log(error)
   }
 }
 
-//wallet connect button
-const connectWalletButton = ()=>{
-  return(
-    <button onClick={connectWalletHandler}>Connect Wallet</button>
-  )
-}
+
+// export const updateVoteCount = async () => {
+
+// }
+
+  //get accounts from metamask
+// export const getAccounts = async () => {
+//   const accounts = await ethereum.request({ method: 'eth_accounts' });
+//   if (accounts.length != 0) {
+//     const account = await accounts[0]
+//     return account;
+//   } else {
+//     console.log('No authorized account found');
+//   }
+// }
 
 
+// export const currentVoteCount = (props) =>{
+//     let candidate1 = props.candidateLookup(1)
+//     return candidate1[1]
+// }
 
-//connect to Contract
-const load = async ()=>{
-  try {
-    if (ethereum) {
-      provider = new ethers.providers.Web3Provider(ethereum);
-      signer = provider.getSigner();
-      contract = new ethers.Contract( ADDRESS, ABI, signer);
-      //Interact with Contract
-      // const candidates = await voteContract.getCandidates();
-      // console.log(candidates[1][0].toNumber())
-      // console.log(candidates[1][1].toNumber())
-      // console.log(candidates[0][0])
-      // console.log(candidates[0][1])
+// export const connectWallet = async () =>{
 
-      // const canTest = await voteContract.candidateLookup(2)
-      // console.log(canTest[2].toNumber())
+// }
 
-      //get info from contract -2 candidates
-      candidate1 = await contract.candidateLookup(1)
-      candidate2 = await contract.candidateLookup(2)
-      console.log(candidate1[0].toNumber(), candidate1[1], candidate1[2].toNumber())
-      console.log(candidate2[0].toNumber(), candidate2[1], candidate2[2].toNumber())
-      // setContractConnected(true);
-      return await candidate1
-      // voteContract.vote(2)
-    }else{
-      console.log("No wallet connected")
-    }
-  } catch (error) {
-    console.log(error)
-  }
-}
+// const getCurrentWalletConnected = async () =>{
+
+// }
 
 
 
 
-//connect to contract button
-const loadButton = ()=>{
-  return(
-    <button onClick={load}>Load Contract</button>
-  )
-}
+// ///////////////////////////////////////////////////////////////////
 
-//Vote function
 
-useEffect(()=>{checkWalletIsConnected()}, [])
+// const [currentAccount, setCurrentAccount] = useState(null);
+// const [contractConnected, setContractConnected] = useState(true);
+
+
+
+
+// //wallet connect button
+// const connectWalletButton = ()=>{
+//   return(
+//     <button onClick={connectWalletHandler}>Connect Wallet</button>
+//   )
+// }
+
+
+
+// //connect to Contract
+// const load = async ()=>{
+//   try {
+//     if (ethereum) {
+//       provider = new ethers.providers.Web3Provider(ethereum);
+//       signer = provider.getSigner();
+//       contract = new ethers.Contract( ADDRESS, ABI, signer);
+//       //Interact with Contract
+//       // const candidates = await voteContract.getCandidates();
+//       // console.log(candidates[1][0].toNumber())
+//       // console.log(candidates[1][1].toNumber())
+//       // console.log(candidates[0][0])
+//       // console.log(candidates[0][1])
+
+//       // const canTest = await voteContract.candidateLookup(2)
+//       // console.log(canTest[2].toNumber())
+
+//       //get info from contract -2 candidates
+//       candidate1 = await contract.candidateLookup(1)
+//       candidate2 = await contract.candidateLookup(2)
+//       console.log(candidate1[0].toNumber(), candidate1[1], candidate1[2].toNumber())
+//       console.log(candidate2[0].toNumber(), candidate2[1], candidate2[2].toNumber())
+//       // setContractConnected(true);
+//       return await candidate1
+//       // voteContract.vote(2)
+//     }else{
+//       console.log("No wallet connected")
+//     }
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+
+
+
+// //connect to contract button
+// const loadButton = ()=>{
+//   return(
+//     <button onClick={load}>Load Contract</button>
+//   )
+// }
+
+// //Vote function
+
+// useEffect(()=>{checkWalletIsConnected()}, [])
